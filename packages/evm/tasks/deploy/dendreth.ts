d31ebce07e3a53e1b354cb84abde5c584f855f88
Dimo99 <dimodimov1999@gmail.com>
2024-08-20 18:04:36 +0300

    add sourcechainid to deployment script

--- packages/evm/tasks/deploy/dendreth.ts
+++ packages/evm/tasks/deploy/dendreth.ts
@@ -6,12 +6,13 @@ import { verify } from "."
 
 task("deploy:DendrETH")
   .addParam("lightclient", "address of the the light client contract", undefined, types.string)
+  .addParam("sourcechainid", "source chain ID", undefined, types.int)
   .addFlag("verify", "whether to verify the contract on Etherscan")
   .setAction(async function (taskArguments: TaskArguments, hre) {
     console.log("Deploying DendrETH adapter...")
     const signers: SignerWithAddress[] = await hre.ethers.getSigners()
     const DendrETHAdapter = await hre.ethers.getContractFactory("DendrETHAdapter")
-    const constructorArguments = [taskArguments.lightclient] as const
+    const constructorArguments = [taskArguments.sourcechainid, taskArguments.lightclient] as const
     const dendrethAdapter = await DendrETHAdapter.connect(signers[0]).deploy(...constructorArguments)
     await dendrethAdapter.deployed()
     console.log("DendrETH adapter deployed to:", dendrethAdapter.address)
